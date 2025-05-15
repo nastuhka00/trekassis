c7361086c2d71e2e29186add21bc81796248a979
nastuhka00 <staygreb@gmail.com>
2025-05-15 16:52:18 +0500

    ura

--- bot/keyb.py
+++ bot/keyb.py
@@ -1,16 +1,8 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from reqwest import get_barbers, get_services
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.filters import Command, StateFilter, CommandStart
-from aiogram.fsm.storage.memory import MemoryStorage
-import logging
-import asyncio
-from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
 from dispecher1 import dp
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, Message, CallbackQuery
+
 
 @dp.message(Command("start"))
 async def send_welcome(message: types.Message):
--- bot/states.py
+++ bot/states.py
@@ -3,3 +3,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 class Work(StatesGroup):
     process = State()
+class Work(StatesGroup):
+    process = State()
+    waiting_for_photo = State()
+    waiting_for_meme_text = State()
